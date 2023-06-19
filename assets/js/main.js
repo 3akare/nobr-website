6dcfe61a6699f85f2a5ccb99691de65cb19c5bd6
David Bakare <bakaredavid007@gmail.com>
2023-06-19 02:13:30 +0100

    Code refactor. Reused header component. Reconfigured dark mode toggle button. Broke legal page into components. Added additional styling to legal page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,21 @@
+document.body.previousElementSibling.insertAdjacentHTML('beforeend', 
+    `
+    <link rel="shortcut icon" href="/images/header/logo.png">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/font/WEB/css/satoshi.css">
+    `)
+document.body.prepend(document.querySelector('header'));
+document.body.append(document.querySelector('footer'));
+
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
-const htmlBody = document.querySelector('#dark-mode-master');
+const htmlBody = document.querySelector('html');
 
 darkModeToggle.addEventListener('click', () => {
   if (htmlBody.classList.contains('dark')) {
-    darkModeToggle.style.backgroundImage = 'url("../images/moon-svgrepo-com.svg")';
+    darkModeToggle.style.backgroundImage = 'url("../../images/theme/moon.svg")';
     localStorage.theme = 'light';
   } else {
-    darkModeToggle.style.backgroundImage = 'url("../images/sun-svgrepo-com.svg")';
+    darkModeToggle.style.backgroundImage = 'url("../../images/theme/sun.svg")';
     localStorage.theme = 'dark';
   }
   if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
@@ -20,19 +29,10 @@ darkModeToggle.style.backgroundSize = '30px 30px';
 
 window.addEventListener('load', () => {
   if (localStorage.theme === 'dark') {
-    darkModeToggle.style.backgroundImage = 'url("../images/sun-svgrepo-com.svg")';
+    darkModeToggle.style.backgroundImage = 'url("../../images/theme/sun.svg")';
     htmlBody.classList.add('dark');
   } else {
-    darkModeToggle.style.backgroundImage = 'url("../images/moon-svgrepo-com.svg")';
+    darkModeToggle.style.backgroundImage = 'url("../../images/theme/moon.svg")';
     htmlBody.classList.remove('dark');
   }
 });
-
-// const textMessageArea = document.querySelector('#text-message-area');
-// textMessageArea.insertAdjacentHTML('beforeend', `<div class="text-left block h-fit p-2 w-fit max-w-[80%] bg-slate-500/100 mb-2 rounded-sm text-secondary-white font-thin">
-//     Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste praesentium dolore voluptatem optio pariatur ullam ex impedit vero aut voluptate sint, assumenda alias facilis maiores saepe! Provident ut sed expedita totam libero dignissimos consequuntur, nobis illo porro iste maiores dolores inventore ea ipsam quam architecto minima quidem aspernatur reprehenderit facere incidunt accusamus autem ipsa! Recusandae labore nisi, nemo autem cupiditate quisquam aliquam dolore, tempora earum consequatur, explicabo deleniti dolores quod.
-// </div>
-// </div>
-// <div class="h-[100px] w-full">
-
-//                 </div>`);
