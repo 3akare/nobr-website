972c0f1badb65aba94af9925e78c2bfcfcf348e9
David Bakare <bakaredavid007@gmail.com>
2023-07-06 23:12:52 +0100

    placed a bunch of the server side code in an async function to speed up the css load time

--- server.js
+++ server.js
@@ -24,65 +24,77 @@ app.use('/font', express.static(path.resolve(__dirname, 'assets/font')));
 
 app.use('/', require('./server/routes/router'));
 
+async function main(){
+
 const server = app.listen(PORT, () => {
   console.log(`Connected on PORT ${PORT}`);
 });
 
-const io = require('socket.io')(server, {
-  allowEI03: true // Avoiding server mis-match issues
+const io = require("socket.io")(server, {
+  allowEI03: true, // Avoiding server mis-match issues
 });
 
 // track the user joined
 let userConnections = [];
 
 // Check if socket.io is connected with the frontend
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`Socket id is ${socket.id}`);
-  socket.on('userConnect', (data) => {
+  socket.on("userConnect", (data) => {
     console.log(`Logged in user: ${data.displayName}`);
 
     // Add user to userConnections array
     userConnections.push({
       connectionId: socket.id,
-      user_id: data.displayName
+      user_id: data.displayName,
     });
     console.log(`User count: ${userConnections.length}`);
   });
 
-  socket.on('offerSentToRemote', (data) => {
+  socket.on("offerSentToRemote", (data) => {
     // Collect offer from user-1 and send to user-2
-    const offerReceiver = userConnections.find((o) => o.user_id === data.remoteUser);
+    const offerReceiver = userConnections.find(
+      (o) => o.user_id === data.remoteUser
+    );
     if (offerReceiver) {
       console.log(`offer receiver ${offerReceiver.connectionId}`);
-      socket.to(offerReceiver.connectionId).emit('receivedOffer', data);
+      socket.to(offerReceiver.connectionId).emit("receivedOffer", data);
     }
   });
 
-  socket.on('answerSentToUser1', (data) => {
+  socket.on("answerSentToUser1", (data) => {
     // Collect answer from user-2
-    const answerReceiver = userConnections.find((o) => o.user_id === data.receiver);
+    const answerReceiver = userConnections.find(
+      (o) => o.user_id === data.receiver
+    );
     if (answerReceiver) {
       console.log(`answer receiver ${answerReceiver.connectionId}`);
-      socket.to(answerReceiver.connectionId).emit('receivedAnswer', data);
+      socket.to(answerReceiver.connectionId).emit("receivedAnswer", data);
     }
   });
 
-  socket.on('candidateSentToUser', (data) => {
+  socket.on("candidateSentToUser", (data) => {
     // receive ice candidate network information
-    const candidateReceiver = userConnections.find((o) => o.user_id === data.remoteUser);
+    const candidateReceiver = userConnections.find(
+      (o) => o.user_id === data.remoteUser
+    );
     if (candidateReceiver) {
-      console.log(`candidateReceiver user is: ${candidateReceiver.connectionId}`);
-      socket.to(candidateReceiver.connectionId).emit('candidateReceiver', data);
+      console.log(
+        `candidateReceiver user is: ${candidateReceiver.connectionId}`
+      );
+      socket.to(candidateReceiver.connectionId).emit("candidateReceiver", data);
     }
   });
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
     // let disUser = userConnections.find((p) => (p.connectionId = socket.id));
     // if (disUser) {
-    userConnections = userConnections.filter((p) => p.connectionId !== socket.id);
+    userConnections = userConnections.filter(
+      (p) => p.connectionId !== socket.id
+    );
     console.log(
-      'Rest users username are: ',
+      "Rest users username are: ",
       userConnections.map(function (user) {
         return user.user_id;
       })
@@ -90,11 +102,15 @@ io.on('connection', (socket) => {
     // }
   });
 
-  socket.on('remoteUserClosed', (data) => {
+  socket.on("remoteUserClosed", (data) => {
     let closedUser = userConnections.find((o) => o.user_id === data.remoteUser);
     if (closedUser) {
       console.log(`closedUser user is: ${closedUser.connectionId}`);
-      socket.to(closedUser.connectionId).emit('closedRemoteUser', data);
+      socket.to(closedUser.connectionId).emit("closedRemoteUser", data);
     }
-  })
+  });
 });
+
+}
+
+main();
